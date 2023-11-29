14df505140685d2cc4053a486bfbf990b0605a68
Liloiscracked <mr.moodyaljoudy@gmail.com>
2023-11-29 16:49:39 +0300

    push the game

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require("express")
+var bodyParser = require("body-parser")
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+app.get("/",(req,res)=>{
+    res.set({
+        "Allow-access-Allow-Origin": '*'
+    })
+    return res.redirect('public/index.html');
+}).listen(3000);
+
+
+
+console.log("Listening on PORT 3000");
